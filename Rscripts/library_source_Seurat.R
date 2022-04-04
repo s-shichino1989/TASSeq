d4f632f880d0697845fe1416b69a0dd88ae9dff7
s-shichino1989 <86090391+s-shichino1989@users.noreply.github.com>
2022-04-04 16:35:43 +0900

    Add files via upload

--- Rscripts/library_source_Seurat.R
+++ Rscripts/library_source_Seurat.R
@@ -3,11 +3,12 @@
 #of each gene by using mclust package
 #Written by Shigeyuki Shichino, ver0.2 20190603
 
-library(reticulate)
+
 reticulate::use_python(
-  python = "/usr/local/Python3-3.7.2/bin/python3.7",
+  python = "/usr/bin/python3",
   required = TRUE
 )
+library(reticulate)
 py_set_seed(42, disable_hash_randomization = TRUE)
 suppressMessages(library(BiocParallel))
 suppressMessages(library(rDBEC))
@@ -18,8 +19,6 @@ suppressMessages(library(foreach))
 suppressMessages(library(doParallel))
 suppressMessages(library(dplyr))
 suppressMessages(library(tibble))
-suppressMessages(library(tidyr))
-suppressMessages(library(RColorBrewer))
 suppressMessages(library(Matrix))
 suppressMessages(library(mclust))
 suppressMessages(library(Seurat))
@@ -36,8 +35,50 @@ suppressMessages(library(ggplotify))
 suppressMessages(library(ggpubr))
 suppressMessages(library(cowplot))
 suppressMessages(library(pheatmap))
+suppressMessages(library(knitr))
+suppressMessages(library(rmdformats))
+suppressMessages(library(stringr))
+suppressMessages(library(DT))
+suppressMessages(library(cluster))
+suppressMessages(library(parallelDist))
+suppressMessages(library(WGCNA))
+suppressMessages(library(flashClust))
+suppressMessages(library(SingleR))
 suppressMessages(library(ggsci))
-
+suppressMessages(library(qs))
 set.seed(42)
-options(future.globals.maxSize= 10000*1024^3)
+options(future.globals.maxSize= 100000*1024^3)
 
+
+custom_colors <- list()
+colors_dutch <- c(
+  '#FFC312','#C4E538','#12CBC4','#FDA7DF','#ED4C67',
+  '#F79F1F','#A3CB38','#1289A7','#D980FA','#B53471',
+  '#EE5A24','#009432','#0652DD','#9980FA','#833471',
+  '#EA2027','#006266','#1B1464','#5758BB','#6F1E51'
+)
+colors_spanish <- c(
+  '#40407a','#706fd3','#f7f1e3','#34ace0','#33d9b2',
+  '#2c2c54','#474787','#aaa69d','#227093','#218c74',
+  '#ff5252','#ff793f','#d1ccc0','#ffb142','#ffda79',
+  '#b33939','#cd6133','#84817a','#cc8e35','#ccae62'
+)
+colors_custom = c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
+                  '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928',
+                  '#49beaa', '#611c35', '#2708a0')
+colors_temp = pal_igv(palette = "default")
+colors_temp = colors_temp(51)
+custom_colors$discrete <- unique(c(colors_dutch, colors_spanish, colors_custom, colors_temp))
+
+
+quiet <- function(x) {
+  sink(tempfile())
+  on.exit(sink())
+  invisible(force(x))
+}
+
+tableread_fast_hashtag = function(x, sep="\t", header=TRUE){
+  tmp = data.table::fread(x, header=header, sep=sep, quote="")
+  tmp = as.data.frame(tmp)
+  return(tmp)
+}
--- Rscripts/setup.R
+++ Rscripts/setup.R
@@ -3,23 +3,20 @@
 
 install.packages(c("data.table", "dplyr", "ggplot2", 
 "stringr", "tibble", "tidyr", "mcgv", "voxel", "ggplotify","foreach", "doParallel", "withr",
-"Matrix", "mclust", "recommenderlab", "MASS", "future.apply", "future", "doFuture", "reticulate", "viridis", "devtools", "factoextra", "flashClust", "cowplot", "XML", "RCurl"), lib="/usr/local/lib/R/site-library")
+"Matrix", "mclust", "recommenderlab", "MASS", "future.apply", "future", "doFuture", 
+"reticulate", "viridis", "devtools", "factoextra", "flashClust", "cowplot", "XML", "RCurl", "Seurat"))
 
-#install bioconductor packages for R 3.6.3
+#install bioconductor packages
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install(version="3.10")
-BiocManager::install(c("SingleCellExperiment","SummarizedExperiment", "GenomicRanges", "TCC", "impute", "preprocessCore", "GO.db", "AnnotationDbi", "TCC", "BiocParallel", "AnnotationDbi", "biomaRt"), lib="/usr/local/lib/R/site-library")
+#BiocManager::install(version="3.10")
+BiocManager::install(c("SingleCellExperiment","SummarizedExperiment", "GenomicRanges", "org.Mm.eg.db", "org.Hs.eg.db",
+                       "TCC", "impute", "preprocessCore", "GO.db", "AnnotationDbi", "TCC", "BiocParallel", 
+                       "AnnotationDbi", "biomaRt", "flowDensity"))
 
-BiocManager::install(c("DropletUtils","flowTrans", "slingshot", "clusterExperiment",
-"monocle", "multtest", "clusterProfiler"), lib="/usr/local/lib/R/site-library")
+BiocManager::install(c("DropletUtils","flowTrans", "slingshot", "clusterExperiment", 
+"monocle", "multtest", "clusterProfiler", "pcaMethods", "BiocNeighbors", "SingleR", "ComplexHeatmap"))
 
-library(devtools)
-library(withr)
-
-withr::with_libpaths(new="/usr/local/lib/R/site-library", install_version("RcppAnnoy", version = "0.0.16")) #need downgrade RcppAnnoy for install BiocNeighbors
-BiocManager::install('BiocNeighbors', type = 'source', lib="/usr/local/lib/R/site-library")
-BiocManager::install(c("SingleR"), version="3.10", lib="/usr/local/lib/R/site-library")
 #if you use CentOS7 you must install hdf5 manually
 #wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.20/src/hdf5-1.8.20.tar.gz
 #unpigz hdf5-1.8.20.tar.gz
@@ -31,20 +28,19 @@ BiocManager::install(c("SingleR"), version="3.10", lib="/usr/local/lib/R/site-li
 #sudo make install
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/hdf5/lib
 #install.packages("hdf5r", configure.args="--with-hdf5=/usr/local/hdf5/bin/h5cc")
-withr::with_libpaths(new="/usr/local/lib/R/site-library", install_url("https://cran.r-project.org/src/contrib/Archive/dbplyr/dbplyr_1.3.0.tar.gz"))
-
-install.packages("WGCNA", "parallelDist", "ggsci", lib="/usr/local/lib/R/site-library")
-
-install.packages("./source_file/SDMTools_1.1-221.2.tar.gz", type="source", repos=NULL, lib="/usr/local/lib/R/site-library")
+#nstall.packages("./source_file/SDMTools_1.1-221.2.tar.gz", type="source", repos=NULL, lib="/usr/local/lib/R/site-library")
 
 #install Seurat v2.3.4
-source("https://z.umn.edu/archived-seurat")
+#source("https://z.umn.edu/archived-seurat")
 
-#install modified tradeSeq package
-BiocManager::install("tradeSeq")
-install.packages("./source_file/rDBEC_0.2.3.tar.gz", type="source", repos=NULL, lib="/usr/local/lib/R/site-library")
+install.packages("./source_file/tradeSeq-master.tar.gz", type="source", repos=NULL)
+install.packages(c("WGCNA", "parallelDist", "ggsci"))
 
-withr::with_libpaths(new="/usr/local/lib/R/site-library", install_github('barkasn/fastSave', lib="/usr/local/lib/R/site-library"))
-install.packages(c("rmdformats", "DT", "ggpubr"), lib="/usr/local/lib/R/site-library")
-withr::with_libpaths(new="/usr/local/lib/R/site-library", install_github("LTLA/celldex"))
+library(devtools)
+install_github('barkasn/fastSave')
+install.packages(c("rmdformats", "DT", "ggpubr", "doRNG", "qs", "dbplyr", "magick", "circlize", "ggnewscale", "msigdbr", "tidyverse", 
+                   "minidown", "extrafont"))
+
+install.packages("./source_file/rDBEC_0.3.1.tar.gz", type="source", repos=NULL)
+install_github("velocyto-team/velocyto.R")
 
